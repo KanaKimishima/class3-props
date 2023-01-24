c0a008cbc8063c3778d6f9f6cdeec20ea7986cb6
KanaKimishima <kana.kimishima.work@gmail.com>
2023-01-23 23:48:22 -0800

    files and folders

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+export default function About() {
+    return(
+        <>
+        <div class="main">
+            <h1>Our Members</h1>
+
+            <div className="kirua">
+                <img></img>
+              <p>Kirua</p>
+            </div>
+
+            <div className="gon">
+              <></>
+            </div>
+        </div>
+        
+        </>
+
+    )
+}
